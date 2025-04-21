1195909ede9669ceb65fe30f40821ffd1cad147e
RajaMahanty <rajamahanty2@gmail.com>
2025-04-22 00:26:34 +0530

    Initial commit

---
+++ client/src/components/gallery/gallery.jsx
@@ -0,0 +1,17 @@
+import "./gallery.css";
+
+// TEMP
+const items = [
+	{
+		id: 1,
+		media: "/pins/pin1.jpg",
+		width: 1260,
+		height: 100,
+	},
+];
+
+const Gallery = () => {
+	return <div className="gallery">Gallery</div>;
+};
+
+export default Gallery;
